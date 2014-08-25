3cc223ddcfc83b0f886b054cfb4e39a531ea7c2b
Vladimir Ermakov <vooon341@gmail.com>
2014-08-25 22:10:08 +0400

    Rebase from 'release/hydro/mavros'

--- src/plugins/setpoint_velocity.cpp
+++ src/plugins/setpoint_velocity.cpp
@@ -1,7 +1,7 @@
 /**
  * @brief SetpointVelocity plugin
  * @file setpoint_velocity.cpp
- * @author Nuno Marques
+ * @author Nuno Marques <n.marques21@hotmail.com>
  * @author Vladimir Ermakov <vooon341@gmail.com>
  *
  * @addtogroup plugin
@@ -26,11 +26,10 @@
  */
 
 #include <mavros/mavros_plugin.h>
+#include <mavros/setpoint_mixin.h>
 #include <pluginlib/class_list_macros.h>
 
-#include <geometry_msgs/Twist.h>
-
-#include "setpoint_mixin.h"
+#include <geometry_msgs/TwistStamped.h>
 
 namespace mavplugin {
 
@@ -61,11 +60,8 @@ public:
 		return "SetpointVelocity";
 	}
 
-	const std::vector<uint8_t> get_supported_messages() const {
+	const message_map get_rx_handlers() {
 		return { /* Rx disabled */ };
-	}
-
-	void message_rx_cb(const mavlink_message_t *msg, uint8_t sysid, uint8_t compid) {
 	}
 
 private:
@@ -82,26 +78,31 @@ private:
 	 *
 	 * Note: send only VX VY VZ. ENU frame.
 	 */
-	void send_setpoint_velocity(float vx, float vy, float vz) {
+	void send_setpoint_velocity(const ros::Time &stamp, float vx, float vy, float vz, float yaw_rate) {
 
 		/* Documentation start from bit 1 instead 0,
-		 * but implementation PX4 Firmware #1151 starts from 0
+		 * Ignore position and accel vectors, yaw
 		 */
-		uint16_t ignore_all_except_v_xyz = (7<<6)|(7<<0);
+		uint16_t ignore_all_except_v_xyz_yr = (1<<10)|(7<<6)|(7<<0);
 
 		// ENU->NED. Issue #49.
-		set_position_target_local_ned(ros::Time::now().toNSec() / 1000000,
+		set_position_target_local_ned(stamp.toNSec() / 1000000,
 				MAV_FRAME_LOCAL_NED,
-				ignore_all_except_v_xyz,
+				ignore_all_except_v_xyz_yr,
 				0.0, 0.0, 0.0,
 				vy, vx, -vz,
-				0.0, 0.0, 0.0);
+				0.0, 0.0, 0.0,
+				0.0, yaw_rate);
 	}
 
 	/* -*- callbacks -*- */
 
-	void vel_cb(const geometry_msgs::Twist::ConstPtr &req) {
-		send_setpoint_velocity(req->linear.x, req->linear.y, req->linear.z);
+	void vel_cb(const geometry_msgs::TwistStamped::ConstPtr &req) {
+		send_setpoint_velocity(req->header.stamp,
+					req->twist.linear.x,
+					req->twist.linear.y,
+					req->twist.linear.z,
+					req->twist.angular.z);
 	}
 };
 
