0b18d6630186e6771542c35f7a105a30086f09ca
Vladimir Ermakov <vooon341@gmail.com>
2014-11-02 21:18:07 +0300

    Rebase from 'upstream'

--- libmavconn/include/mavconn/serial.h
+++ libmavconn/include/mavconn/serial.h
@@ -1,13 +1,13 @@
 /**
- * @brief MAVConn UDP link class
- * @file mavconn_udp.h
+ * @brief MAVConn Serial link class
+ * @file serial.h
  * @author Vladimir Ermakov <vooon341@gmail.com>
  *
  * @addtogroup mavconn
  * @{
  */
 /*
- * Copyright 2013 Vladimir Ermakov.
+ * Copyright 2013,2014 Vladimir Ermakov.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -29,28 +29,25 @@
 #include <list>
 #include <atomic>
 #include <boost/asio.hpp>
-#include <mavros/mavconn_interface.h>
-#include <mavros/mavconn_msgbuffer.h>
+#include <mavconn/interface.h>
+#include <mavconn/msgbuffer.h>
 
 namespace mavconn {
 
 /**
- * @brief UDP interface
- *
- * @note IPv4 only
+ * @brief Serial interface
  */
-class MAVConnUDP : public MAVConnInterface {
+class MAVConnSerial : public MAVConnInterface {
 public:
 	/**
-	 * @param[id] bind_host    bind host
-	 * @param[id] bind_port    bind port
-	 * @param[id] remote_host  remote host (optional)
-	 * @param[id] remote_port  remote port (optional)
+	 * Open and run serial link.
+	 *
+	 * @param[in] device    TTY device path
+	 * @param[in] baudrate  serial baudrate
 	 */
-	MAVConnUDP(uint8_t system_id = 1, uint8_t component_id = MAV_COMP_ID_UDP_BRIDGE,
-			std::string bind_host = "localhost", unsigned short bind_port = 14555,
-			std::string remote_host = "", unsigned short remote_port = 14550);
-	~MAVConnUDP();
+	MAVConnSerial(uint8_t system_id = 1, uint8_t component_id = MAV_COMP_ID_UDP_BRIDGE,
+			std::string device = "/dev/ttyACM0", unsigned baudrate = 57600);
+	~MAVConnSerial();
 
 	void close();
 
@@ -59,28 +56,22 @@ public:
 	void send_bytes(const uint8_t *bytes, size_t length);
 
 	inline mavlink_status_t get_status() { return *mavlink_get_channel_status(channel); };
-	inline bool is_open() { return socket.is_open(); };
+	inline bool is_open() { return serial_dev.is_open(); };
 
 private:
 	boost::asio::io_service io_service;
-	std::unique_ptr<boost::asio::io_service::work> io_work;
 	std::thread io_thread;
-
-	std::atomic<bool> remote_exists;
-	boost::asio::ip::udp::socket socket;
-	boost::asio::ip::udp::endpoint remote_ep;
-	boost::asio::ip::udp::endpoint last_remote_ep;
-	boost::asio::ip::udp::endpoint bind_ep;
+	boost::asio::serial_port serial_dev;
 
 	std::atomic<bool> tx_in_progress;
 	std::list<MsgBuffer*> tx_q;
 	uint8_t rx_buf[MsgBuffer::MAX_SIZE];
 	std::recursive_mutex mutex;
 
-	void do_recvfrom();
-	void async_receive_end(boost::system::error_code, size_t bytes_transferred);
-	void do_sendto(bool check_tx_state);
-	void async_sendto_end(boost::system::error_code, size_t bytes_transferred);
+	void do_read();
+	void async_read_end(boost::system::error_code, size_t bytes_transferred);
+	void do_write(bool check_tx_state);
+	void async_write_end(boost::system::error_code, size_t bytes_transferred);
 };
 
 }; // namespace mavconn
