639fcb290ea6f0bc5c70e930a0c8ed22e2c2e2ee
Vladimir Ermakov <vooon341@gmail.com>
2014-11-02 21:13:00 +0300

    Rebase from 'upstream'

--- mavros/src/lib/rosconsole_bridge.cpp
+++ mavros/src/lib/rosconsole_bridge.cpp
@@ -1,10 +1,7 @@
 /**
- * @brief Mavlink diag class
- * @file mavlink_diag.h
+ * @brief Enable console bridge
+ * @file rosconsole_bridge.cpp
  * @author Vladimir Ermakov <vooon341@gmail.com>
- *
- * @addtogroup nodelib
- * @{
  */
 /*
  * Copyright 2014 Vladimir Ermakov.
@@ -24,33 +21,6 @@
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
-#pragma once
+#include <rosconsole_bridge/bridge.h>
 
-#include <diagnostic_updater/diagnostic_updater.h>
-#include <mavconn/interface.h>
-
-namespace mavros {
-
-class MavlinkDiag : public diagnostic_updater::DiagnosticTask
-{
-public:
-	explicit MavlinkDiag(std::string name);
-
-	void run(diagnostic_updater::DiagnosticStatusWrapper &stat);
-
-	void set_mavconn(const mavconn::MAVConnInterface::Ptr &link) {
-		weak_link = link;
-	}
-
-	void set_connection_status(bool connected) {
-		is_connected = connected;
-	}
-
-private:
-	mavconn::MAVConnInterface::WeakPtr weak_link;
-	unsigned int last_drop_count;
-	bool is_connected;
-};
-
-}; // namespace mavros
-
+REGISTER_ROSCONSOLE_BRIDGE;
