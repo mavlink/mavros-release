19c5088662dcef9f9c07a483e56fb5c62ac6b3fd
Vladimir Ermakov <vooon341@gmail.com>
2014-11-03 12:14:02 +0300

    Revert "Trimmed the branch to only the libmavconn sub directory"
    This reverts commit 19f105c2a11e65c79285f923e238b6d40bce5a79.

---
+++ mavros/src/mavros_node.cpp
@@ -0,0 +1,36 @@
+/**
+ * @brief MAVROS Node
+ * @file mavros_node.cpp
+ * @author Vladimir Ermakov <vooon341@gmail.com>
+ */
+/*
+ * Copyright 2013 Vladimir Ermakov.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include <mavros/mavros.h>
+
+int main(int argc, char *argv[])
+{
+	ros::init(argc, argv, "mavros");
+	ros::NodeHandle nh("~");
+
+	mavros::MavRos mavros(nh);
+	mavros.spin();
+
+	return 0;
+}
+
