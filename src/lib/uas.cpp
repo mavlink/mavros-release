8291df7ef3d14a586ea5d2c2dbd52949e3e7efce
Vladimir Ermakov <vooon341@gmail.com>
2015-03-24 19:25:59 +0300

    Rebase from 'debian/jade/mavros'

--- src/lib/uas.cpp
+++ src/lib/uas.cpp
@@ -6,21 +6,13 @@
 /*
  * Copyright 2014 Vladimir Ermakov.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- * for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * This file is part of the mavros package and subject to the license terms
+ * in the top-level LICENSE file of the mavros repository.
+ * https://github.com/mavlink/mavros/tree/master/LICENSE.md
  */
 
+#include <array>
+#include <unordered_map>
 #include <stdexcept>
 #include <mavros/mavros_uas.h>
 #include <mavros/utils.h>
@@ -34,28 +26,44 @@ UAS::UAS() :
 	target_system(1),
 	target_component(1),
 	connected(false),
-	angular_velocity(),
-	linear_acceleration(),
-	orientation(),
-	gps_latitude(0),
-	gps_longitude(0),
-	gps_altitude(0),
-	gps_eph(0),
-	gps_epv(0),
-	fix_status(0)
-{
-}
+	imu_orientation(),
+	imu_angular_velocity(),
+	imu_linear_acceleration(),
+	gps_eph(NAN),
+	gps_epv(NAN),
+	gps_fix_type(0),
+	gps_satellites_visible(0),
+	fcu_caps_known(false),
+	fcu_capabilities(0)
+{}
 
 void UAS::stop(void)
+{}
+
+
+/* -*- heartbeat handlers -*- */
+
+void UAS::update_heartbeat(uint8_t type_, uint8_t autopilot_)
 {
+	type = type_;
+	autopilot = autopilot_;
 }
+
+void UAS::update_connection_status(bool conn_)
+{
+	if (conn_ != connected) {
+		connected = conn_;
+		sig_connection_changed(connected);
+	}
+}
+
 
 /* -*- time syncronise functions -*- */
 
 static inline ros::Time ros_time_from_ns(uint64_t &stamp_ns) {
 	return ros::Time(
-			stamp_ns / 1000000000UL,	// t_sec
-			stamp_ns % 1000000000UL);	// t_nsec
+		stamp_ns / 1000000000UL,		// t_sec
+		stamp_ns % 1000000000UL);		// t_nsec
 }
 
 ros::Time UAS::synchronise_stamp(uint32_t time_boot_ms) {
@@ -81,9 +89,100 @@ ros::Time UAS::synchronise_stamp(uint64_t time_usec) {
 		return ros::Time::now();
 }
 
+
+/* -*- autopilot version -*- */
+
+static uint64_t get_default_caps(uint8_t ap_type)
+{
+	// TODO: return default caps mask for known FCU's
+	return 0;
+}
+
+uint64_t UAS::get_capabilities()
+{
+	if (fcu_caps_known) {
+		uint64_t caps = fcu_capabilities;
+		return caps;
+	}
+	else {
+		return get_default_caps(get_autopilot());
+	}
+}
+
+void UAS::update_capabilities(bool known, uint64_t caps)
+{
+	fcu_caps_known = known;
+	fcu_capabilities = caps;
+}
+
+
+/* -*- IMU data -*- */
+
+void UAS::update_attitude_imu(tf::Quaternion &q, tf::Vector3 &av, tf::Vector3 &lacc)
+{
+	lock_guard lock(mutex);
+
+	imu_orientation = q;
+	imu_angular_velocity = av;
+	imu_linear_acceleration = lacc;
+}
+
+tf::Vector3 UAS::get_attitude_angular_velocity()
+{
+	lock_guard lock(mutex);
+	return imu_angular_velocity;
+}
+
+tf::Vector3 UAS::get_attitude_linear_acceleration()
+{
+	lock_guard lock(mutex);
+	return imu_linear_acceleration;
+}
+
+tf::Quaternion UAS::get_attitude_orientation()
+{
+	lock_guard lock(mutex);
+	return imu_orientation;
+}
+
+
+/* -*- GPS data -*- */
+
+void UAS::update_gps_fix_epts(sensor_msgs::NavSatFix::Ptr &fix,
+		float eph, float epv,
+		int fix_type, int satellites_visible)
+{
+	lock_guard lock(mutex);
+
+	gps_fix = fix;
+	gps_eph = eph;
+	gps_epv = epv;
+	gps_fix_type = fix_type;
+	gps_satellites_visible = satellites_visible;
+}
+
+//! Returns EPH, EPV, Fix type and satellites visible
+void UAS::get_gps_epts(float &eph, float &epv, int &fix_type, int &satellites_visible)
+{
+	lock_guard lock(mutex);
+
+	eph = gps_eph;
+	epv = gps_epv;
+	fix_type = gps_fix_type;
+	satellites_visible = gps_satellites_visible;
+}
+
+//! Retunrs last GPS RAW message
+sensor_msgs::NavSatFix::Ptr UAS::get_gps_fix()
+{
+	lock_guard lock(mutex);
+	return gps_fix;
+}
+
+
 /* -*- mode stringify functions -*- */
 
-typedef std::map<uint32_t, std::string> cmode_map;
+typedef std::unordered_map<uint32_t, const std::string> cmode_map;
 
 /** APM:Plane custom mode -> string
  *
@@ -196,10 +295,10 @@ static inline std::string str_mode_px4(uint32_t custom_mode_int) {
 
 static inline bool is_apm_copter(enum MAV_TYPE &type) {
 	return type == MAV_TYPE_QUADROTOR ||
-		type == MAV_TYPE_HEXAROTOR ||
-		type == MAV_TYPE_OCTOROTOR ||
-		type == MAV_TYPE_TRICOPTER ||
-		type == MAV_TYPE_COAXIAL;
+	       type == MAV_TYPE_HEXAROTOR ||
+	       type == MAV_TYPE_OCTOROTOR ||
+	       type == MAV_TYPE_TRICOPTER ||
+	       type == MAV_TYPE_COAXIAL;
 }
 
 std::string UAS::str_mode_v10(uint8_t base_mode, uint32_t custom_mode) {
@@ -277,3 +376,96 @@ bool UAS::cmode_from_str(std::string cmode_str, uint32_t &custom_mode) {
 	ROS_ERROR_NAMED("uas", "MODE: Unsupported FCU");
 	return false;
 }
+
+/* -*- enum stringify -*- */
+
+//! MAV_AUTOPILOT values
+static const std::array<const std::string, 18> autopilot_strings = {
+	/*  0 */ "Generic",
+	/*  1 */ "PIXHAWK",
+	/*  2 */ "SLUGS",
+	/*  3 */ "ArduPilotMega",
+	/*  4 */ "OpenPilot",
+	/*  5 */ "Generic-WP-Only",
+	/*  6 */ "Generic-WP-Simple-Nav",
+	/*  7 */ "Generic-Mission-Full",
+	/*  8 */ "INVALID",
+	/*  9 */ "Paparazzi",
+	/* 10 */ "UDB",
+	/* 11 */ "FlexiPilot",
+	/* 12 */ "PX4",
+	/* 13 */ "SMACCMPILOT",
+	/* 14 */ "AUTOQUAD",
+	/* 15 */ "ARMAZILA",
+	/* 16 */ "AEROB",
+	/* 17 */ "ASLUAV"
+};
+
+std::string UAS::str_autopilot(enum MAV_AUTOPILOT ap)
+{
+	size_t idx = size_t(ap);
+	if (idx >= autopilot_strings.size())
+		return std::to_string(idx);
+
+	return autopilot_strings[idx];
+}
+
+static const std::array<const std::string, 27> type_strings = {
+	/*  0 */ "Generic",
+	/*  1 */ "Fixed-Wing",
+	/*  2 */ "Quadrotor",
+	/*  3 */ "Coaxial",
+	/*  4 */ "Helicopter",
+	/*  5 */ "Antenna-Tracker",
+	/*  6 */ "GCS",
+	/*  7 */ "Airship",
+	/*  8 */ "Free-Balloon",
+	/*  9 */ "Rocket",
+	/* 10 */ "Ground-Rover",
+	/* 11 */ "Surface-Boat",
+	/* 12 */ "Submarine",
+	/* 13 */ "Hexarotor",
+	/* 14 */ "Octorotor",
+	/* 15 */ "Tricopter",
+	/* 16 */ "Flapping-Wing",
+	/* 17 */ "Kite",
+	/* 18 */ "Onboard-Controller",
+	/* 19 */ "VTOL-Duorotor",
+	/* 20 */ "VTOL-Quadrotor",
+	/* 21 */ "VTOL-RESERVED1",
+	/* 22 */ "VTOL-RESERVED2",
+	/* 23 */ "VTOL-RESERVED3",
+	/* 24 */ "VTOL-RESERVED4",
+	/* 25 */ "VTOL-RESERVED5",
+	/* 26 */ "Gimbal"
+};
+
+std::string UAS::str_type(enum MAV_TYPE type)
+{
+	size_t idx = size_t(type);
+	if (idx >= type_strings.size())
+		return std::to_string(idx);
+
+	return type_strings[idx];
+}
+
+static const std::array<const std::string, 8> state_strings = {
+	/*  0 */ "Uninit",
+	/*  1 */ "Boot",
+	/*  2 */ "Calibrating",
+	/*  3 */ "Standby",
+	/*  4 */ "Active",
+	/*  5 */ "Critical",
+	/*  6 */ "Emergency",
+	/*  7 */ "Poweroff"
+};
+
+std::string UAS::str_system_status(enum MAV_STATE st)
+{
+	size_t idx = size_t(st);
+	if (idx >= state_strings.size())
+		return std::to_string(idx);
+
+	return state_strings[idx];
+}
+
