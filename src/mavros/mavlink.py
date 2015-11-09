c2dc067a64c4085f0325493609220d71433fc52d
Vladimir Ermakov <vooon341@gmail.com>
2015-11-09 14:07:32 +0300

    Rebase from 'rpm/indigo/mavros'

--- src/mavros/mavlink.py
+++ src/mavros/mavlink.py
@@ -7,9 +7,12 @@
 # in the top-level LICENSE file of the mavros repository.
 # https://github.com/mavlink/mavros/tree/master/LICENSE.md
 
+import rospy
 import struct
 from pymavlink import mavutil
 from pymavlink.generator.mavcrc import x25crc
+from mavros_msgs.msg import Mavlink
+from std_msgs.msg import Header
 
 
 def convert_to_bytes(msg):
@@ -47,3 +50,39 @@ def convert_to_bytes(msg):
     # finalize
     msgdata += struct.pack('<H', crc16)
     return msgdata
+
+
+def convert_to_payload64(payload_bytes):
+    """
+    Convert payload bytes to Mavlink.payload64
+    """
+    payload_bytes = bytearray(payload_bytes)
+    payload_len = len(payload_bytes)
+    payload_octets = payload_len / 8
+    if payload_len % 8 > 0:
+        payload_octets += 1
+        payload_bytes += b'\0' * (8 - payload_len % 8)
+
+    return struct.unpack('<%dQ' % payload_octets, payload_bytes)
+
+
+def convert_to_rosmsg(mavmsg, stamp=None):
+    """
+    Convert pymavlink message to Mavlink.msg
+    """
+    if stamp is not None:
+        header = Header(stamp=stamp)
+    else:
+        header = Header(stamp=rospy.get_rostime())
+
+    return Mavlink(
+        header=header,
+        is_valid=True,
+        len=len(mavmsg.get_payload()),
+        seq=mavmsg.get_seq(),
+        sysid=mavmsg.get_srcSystem(),
+        compid=mavmsg.get_srcComponent(),
+        msgid=mavmsg.get_msgId(),
+        checksum=mavmsg.get_crc(),
+        payload64=convert_to_payload64(mavmsg.get_payload())
+    )
