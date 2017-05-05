3fea7d47c0eaa212567aef9d22d2c106f3db8649
Vladimir Ermakov <vooon341@gmail.com>
2017-05-05 06:36:12 +0000

    Rebase from 'release/kinetic/mavros'

--- include/mavros/utils.h
+++ include/mavros/utils.h
@@ -36,6 +36,18 @@ namespace utils {
 using mavconn::utils::format;
 
 /**
+ * Possible modes of timesync operation
+ *
+ * Used by UAS class, but it can't be defined inside because enum is used in utils.
+ */
+enum class timesync_mode {
+	NONE = 0,	//!< Disabled
+	MAVLINK,	//!< Via TIMESYNC message
+	ONBOARD,
+	PASSTHROUGH,
+};
+
+/**
  * Helper to get enum value from strongly typed enum (enum class).
  */
 template<typename _T>
@@ -45,6 +57,11 @@ constexpr typename std::underlying_type<_T>::type enum_value(_T e)
 }
 
 /**
+ * Get string repr for timesync_mode
+ */
+std::string to_string(timesync_mode e);
+
+/**
  * @brief Retrieve alias of the orientation received by MAVLink msg.
  */
 std::string to_string(mavlink::common::MAV_SENSOR_ORIENTATION e);
@@ -52,6 +69,8 @@ std::string to_string(mavlink::common::MAV_SENSOR_ORIENTATION e);
 std::string to_string(mavlink::common::MAV_AUTOPILOT e);
 std::string to_string(mavlink::common::MAV_TYPE e);
 std::string to_string(mavlink::common::MAV_STATE e);
+std::string to_string(mavlink::common::ADSB_ALTITUDE_TYPE e);
+std::string to_string(mavlink::common::ADSB_EMITTER_TYPE e);
 
 /**
  * Helper to call to_string() for enum _T
@@ -73,6 +92,10 @@ Eigen::Quaterniond sensor_orientation_matching(mavlink::common::MAV_SENSOR_ORIEN
  */
 int sensor_orientation_from_str(const std::string &sensor_orientation);
 
+/**
+ * @brief Retreive timesync mode from name
+ */
+timesync_mode timesync_mode_from_str(const std::string &mode);
 
 }	// namespace utils
 }	// namespace mavros
